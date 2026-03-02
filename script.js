2fcc169f289a663d27406c67067201ac235d50c2
js-2 <js-2@gm.cm>
2026-03-02 12:10:23 +0500

    sy

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-var age = +prompt()
+var age = +prompt("введите свой возраст")
+if (age > 0 && age <= 18) {
+    alert("Вы еще молоды, Вам нужно учиться")
+} else if (age > 18 && age <= 50) {
+    alert("Вы еще молоды, Вам нужно учиться")
+} else if (age > 50 && age <= 59) {
+    alert("Вы еще молоды, Вам нужно учиться")
+} else if (age > 59 && age <= 100) {
+    alert("Вы еще молоды, Вам нужно учиться")
+} else {
+    alert("что то пошло не так")
+}
+
+
+var time = +prompt("введите время")
+if (time > 0 && time <= 6) {
+    alert(time + " часа ночи")
+} else if (time > 7 && time <= 10) {
+    alert(time + " часов утра")
+} else if (time > 11 && time <= 17) {
+    alert(time + " часа дня")
+} else if (time > 18 && time <= 23) {
+    alert(time + "  часов вечера")
+}
+
+var a = +prompt("введите первое число")
+var b = +prompt("введите второе число")
+var c = +prompt("введите третье число")
+    
